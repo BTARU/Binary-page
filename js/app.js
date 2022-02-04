bb6339fc0fbb10b880930b4a7f5a52efc7396940
Bulat <87946133+BTARU@users.noreply.github.com>
2022-02-04 20:01:23 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,24 +1,23 @@
-'use strict'
+"use strict";
 
-let menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-let scrollArrow = document.querySelector('.mainblock__arrow[data-goto]');
+let scrollArrow = document.querySelector(".mainblock__arrow[data-goto]");
+if (scrollArrow) scrollArrow.addEventListener("click", onMenuLinkClick);
 
+const menuLinks = document.querySelectorAll(".menu__link[data-goto]");
 if (menuLinks.length > 0) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener('click', onMenuLinkClick);
-    });    
+    menuLinks.forEach((menuLink) => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
 }
-
-scrollArrow.addEventListener('click', onMenuLinkClick);
 
 function onMenuLinkClick(e) {
     let menuLink = e.target;
     if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
         let gotoBlock = document.querySelector(menuLink.dataset.goto);
-        let gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY;         
+        let gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY;
         window.scrollTo({
             top: gotoBlockValue,
-            behavior: 'smooth'
+            behavior: "smooth",
         });
         e.preventDefault();
     }
